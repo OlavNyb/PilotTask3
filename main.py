107b82bcc64afd1c41d184dd4eecf699b13f9570
Olav <nybo.olav@hotmail.no>
2022-04-25 15:18:15 +0200

    General cleanup

--- main.py
+++ main.py
@@ -21,14 +21,13 @@ def filter_out_motif(rep, motif: str):
 
 
 def simulate_motif_in_repertoires(pos, neg, motif: str, num_of_repertoires: int, repertoire_size: int,
-                                  percentage_pos_in_pos_reps: float):
-
-    random.seed(1)
+                                  percentage_pos_in_pos_reps: float, seed):
+    random.seed(seed)
 
     repertoires = []
     disease_list = []
 
-    num_of_pos_in_pos_repertoires = percentage_pos_in_pos_reps*repertoire_size
+    num_of_pos_in_pos_repertoires = percentage_pos_in_pos_reps * repertoire_size
 
     filtered_neg = filter_out_motif(neg, motif)
 
@@ -36,16 +35,17 @@ def simulate_motif_in_repertoires(pos, neg, motif: str, num_of_repertoires: int,
         new_rep = []
         if x < num_of_repertoires / 2:
             disease_list.append(True)
-            # POSITIVE
+            # POSITIVE REPERTOIRE
             for i in range(int(repertoire_size)):
                 if i < num_of_pos_in_pos_repertoires:
-                    new_rep.append(pos[random.randint(0, len(pos)-1)])
+                    new_rep.append(pos[random.randint(0, len(pos) - 1)])
                 else:
-                    new_rep.append(filtered_neg[random.randint(0, len(filtered_neg)-1)])
+                    new_rep.append(filtered_neg[random.randint(0, len(filtered_neg) - 1)])
         else:
             disease_list.append(False)
+            # NEGATIVE REPERTOIRE
             for i in range(int(repertoire_size)):
-                new_rep.append(filtered_neg[random.randint(0, len(filtered_neg)-1)])
+                new_rep.append(filtered_neg[random.randint(0, len(filtered_neg) - 1)])
 
         repertoires.append(new_rep)
 
@@ -62,47 +62,44 @@ def create_receptor_sequence(seq: str, receptor_id):
 
 
 def create_repertoire(sequence_objects, export_path, repertoire_id):
-    repertoire = Repertoire.build_from_sequence_objects(sequence_objects=sequence_objects, path=export_path, filename_base=repertoire_id,
+    repertoire = Repertoire.build_from_sequence_objects(sequence_objects=sequence_objects, path=export_path,
+                                                        filename_base=repertoire_id,
                                                         metadata={"subject_id": repertoire_id})
     return repertoire
 
 
-def main():
+def main(positive_data_file_name: str, negative_data_file_name: str, given_motif: str, num_of_repertoires: int,
+         repertoire_size: int,
+         percentage_pos_in_pos_reps: float, output: str, seed=1):
     print("STARTED...")
 
-    # MOTIF
-    given_motif = "SEY"
-
     neg_seqs = []
-    with open("SimData/many_seqs.tsv") as file:
+    with open(negative_data_file_name) as file:
         tsv_file = csv.reader(file, delimiter="\t")
         for line in tsv_file:
             neg_seqs.append(line[0])
     file.close()
 
     pos_seqs = []
-    with open("SimData/few_seqs.tsv") as file:
+    with open(positive_data_file_name) as file:
         tsv_file = csv.reader(file, delimiter="\t")
         for line in tsv_file:
             pos_seqs.append(line[0])
     file.close()
 
-    filtered = filter_out_motif(neg_seqs, given_motif)
+    export_path = PathlibPath(os.getcwd()) / (output + "/")
 
-    print(len(filtered), " / ", len(neg_seqs))
-    # return
-
-    export_path = PathlibPath(os.getcwd()) / "airr_exporter_repertoire/"
-
+    # delete path directory if already exists
     if path.exists(export_path):
         shutil.rmtree(export_path)
 
     PathBuilder.build(export_path)
+
+    # create temporary directory tmp
     tmp_path = export_path / "tmp"
     PathBuilder.build(tmp_path)
 
-    reps, disease_list = simulate_motif_in_repertoires(pos_seqs, neg_seqs, given_motif, 100, 1000, 0.5)
-    # reps, disease_list = simulate_motif_in_repertoires(pos_seqs, neg_seqs, given_motif, 2, 100, 0.5)
+    reps, disease_list = simulate_motif_in_repertoires(pos_seqs, neg_seqs, given_motif, num_of_repertoires, repertoire_size, percentage_pos_in_pos_reps, seed)
 
     repertoires = [
         create_repertoire([create_receptor_sequence(seq, i) for i, seq in enumerate(r, start=1)], tmp_path, num) for
@@ -118,10 +115,17 @@ def main():
 
     AIRRExporter.export(dataset, export_path)
 
+    # delete temporary directory tmp
     shutil.rmtree(tmp_path)
 
     print("FINISHED")
 
 
 if __name__ == '__main__':
-    main()
+    positive_data_file_name, negative_data_file_name = ("SimData/few_seqs.tsv", "SimData/many_seqs.tsv")
+    given_motif = "SEY"
+    num_of_repertoires, repertoire_size, percentage_pos_in_pos_reps = (100, 1000, 0.5)
+    output = "airr_exporter_repertoire"
+    seed = 1
+
+    main(positive_data_file_name, negative_data_file_name, given_motif, num_of_repertoires, repertoire_size, percentage_pos_in_pos_reps, output, seed = seed)
